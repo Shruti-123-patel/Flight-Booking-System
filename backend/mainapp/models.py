7e6d299fb2641069250aef8dfa951aace8d693d1
Garima Jain <jgarima203@gmail.com>
2024-10-15 14:47:23 +0530

    login_API

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class LoginActivity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    login_time = models.DateTimeField(default=timezone.now)
+    # ip_address = models.GenericIPAddressField()
+    
+    def __str__(self):
+        return f"{self.user.username} logged in at {self.login_time}"
