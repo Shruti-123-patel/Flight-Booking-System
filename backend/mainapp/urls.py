9029fef7d1f7a869133530215b76f4698d58fa5c
Shruti-123-patel <shruti29032003@gmail.com>
2024-11-05 06:46:38 +0530

    flask setup

--- backend/mainapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path,include
-from .views import get_data
-
-
-urlpatterns = [
-    path('api/data/', get_data ),
-]
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ function App() {
 
   useEffect(() => {
     // Fetch the message from Django
-    fetch('mainapp/api/data/')
+    fetch(`${process.env.REACT_APP_API_URL}/hello`)
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error:', error));
