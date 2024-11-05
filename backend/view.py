4099dc93139cca9f96a7d52f5db3f7345bf4ac7b
Shruti-123-patel <shruti29032003@gmail.com>
2024-11-05 13:54:08 +0530

    dummy added

--- backend/view.py
+++ backend/view.py
@@ -5,8 +5,9 @@ from datetime import datetime, date, time, timedelta
 import app
 import json
 
-def add_flight(From,To,Take_off_time,Duration,Company,Price,Class_flight):
-    new_f = flight(From = From,To = To,Take_off_time = Take_off_time,Duration = Duration,Company =Company )
+def add_flight(From,To,Take_off_time,Duration,Company,Class_flight,Price):
+    Take_off_time_ = datetime.strptime(Take_off_time, '%Y-%m-%dT%H:%M:%S')
+    new_f = flight(From = From,To = To,Take_off_time = Take_off_time_,Duration = Duration,Company =Company )
     db.session.add(new_f)
     db.session.commit()
     new_p = flight_class(Fid = new_f.Fid,Price=Price,Class_flight=Class_flight)
@@ -14,8 +15,12 @@ def add_flight(From,To,Take_off_time,Duration,Company,Price,Class_flight):
     db.session.commit()
 
 def add_flights_from_list(flights):
-    for flight_data in flights:
-        add_flight(flight_data)
+    print(flights)
+    if flights is not None:
+        for flight_data in flights:
+            print(flight_data)
+            # print(**flight_data)
+            add_flight(**flight_data)
 
 def list_from_json(file):
     with open(file, 'r') as file:
